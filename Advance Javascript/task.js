88cdc7e7b7818f9d79b688a515a62cde6668373b
Dev Gandhi <devgandhi1113@gmail.com>
2023-07-17 22:31:44 +0530

    Add files via upload

---
+++ Advance Javascript/task.js
@@ -0,0 +1,30 @@
+let familyData = {
+  name: Sureshchandra,
+  age: 71,
+  son: [
+    {
+      name: Parikshit,
+      age: 46,
+      occupation: Business,
+      vehicalDetails: [{ name: Mahindra, no: 1754 }],
+      son: [{ name: Dev, age: 21 }],
+      son: [{name: Yaksh, age: 10}],
+    },
+    {
+      name: Mehul,
+      age: 43,
+      occupation: Business,
+      vehicalDetails: [{ name: Toyota, no: 9142 }],
+      daughter: [{ name: Ishita, age: 20}],
+      daughter: [{name: Jahnavi, age: 15}],
+    },
+    {
+      name: Paresh,
+      age: 55,
+      occupation: Employee,
+      vehicalDetails: [{ name: Honda, no: 9621 }],
+      daughter: [{ name: Parishi, age: 23 }],
+      daughter: [{name: Freya, age: 16}],
+    }
+  ],
+};
