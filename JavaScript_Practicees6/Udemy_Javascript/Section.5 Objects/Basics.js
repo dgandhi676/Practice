710ec34eea7df5080df3836d0be99117a344a6cf
Dev Gandhi <devgandhi1113@gmail.com>
2023-09-28 18:37:02 +0530

    Add files via upload

---
+++ JavaScript_Practicees6/Udemy_Javascript/Section.5 Objects/Basics.js
@@ -0,0 +1,16 @@
+
+// Object-Oriented Programming (OOP)
+
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    inVisible: true,
+    draw: function () {
+        console.log('draw');
+    }
+}
+
+circle.draw(); //Method
---
+++ JavaScript_Practicees6/Udemy_Javascript/Section.5 Objects/constructorfunction.js
@@ -0,0 +1,21 @@
+
+// // Factory Function
+// function createCircle(radius) {
+//     return {
+//         radius,
+//         draw() {
+//             console.log('draw');
+//         }
+//     };
+// }
+
+
+// Constructor Function
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+}
+
+const circle = new Circle(1);
