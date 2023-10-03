77d8354961574ae081ab62f17f67a6327edfe854
Dev Gandhi <devgandhi1113@gmail.com>
2023-10-03 09:55:24 +0530

    Add files via upload

---
+++ JavaScript_Practicees6/Udemy_Javascript/Section.6 Arrays/8-combinnigandslicingarray.js
@@ -0,0 +1,11 @@
+
+const first = [1, 2, 3];
+const second = [4, 5, 6];
+
+const combined = first.concat(second);
+first[0].id = 10;
+
+const slice = combined.slice();
+
+console.log(combined);
+console.log(slice);
